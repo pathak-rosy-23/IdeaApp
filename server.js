1810c79620b7b511d39b7b55463f3599afb420e9
rosypathak <pathakrosy2327@gmail.com>
2023-08-10 20:06:24 +0530

    Adding the server config

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const serverConfig = require('./configs/server.config');
+
+const app = express();
+
+app.listen(serverConfig.PORT, ()=>{
+    console.log(`server started on the port number ${serverConfig.PORT}`);
+})
