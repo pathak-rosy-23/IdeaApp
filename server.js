9140eef999e1afe383f15983f25389524953faa7
rosypathak <pathakrosy2327@gmail.com>
2023-08-12 20:16:13 +0530

    connecting to MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require("express");
 
 const serverConfig = require('./configs/server.config');
+const mongoose  = require("mongoose");
+const dbConfig = require("./configs/db.config")
+
 
 const app = express();
+
+/**
+ * Logic to connect to MongoDB and create an ADMIN user
+ */
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection ;
+
+
+db.on("error", ()=>{
+    console.log("Error while connecting to DB")
+});
+db.once("open", ()=>{
+    console.log("DB is connected");
+})
+
 
 app.listen(serverConfig.PORT, ()=>{
     console.log(`server started on the port number ${serverConfig.PORT}`);
